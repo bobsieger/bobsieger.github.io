a56a380061cc45402c5f12beb502a3eb309ebc95
Robert Obsieger <obsieger@gmail.com>
2021-02-07 14:52:39 -0800

    Update sketch.js

--- fractaltreedatavis/sketch.js
+++ fractaltreedatavis/sketch.js
@@ -12,11 +12,12 @@
 //jshint esversion:6
 
 // Tree rendering variables
+let fillAlpha = 20; strokeAlpha = 40;       // Determines leaf transparency
 let maxBranchAngle = 30;                    // Used with a random function
-let maxTrunkWidth = 25;                     // Maximum initial trunk size
+let minTrunkWidth = 9; maxTrunkWidth = 25;  // Min/max trunk size
 let maxLeafWidth = 15, maxLeafHeight = 100; // Used with a random function
 let leftReduce = 0.75, rightReduce = 0.75;  // Segment size reduction
-let segmentMin = 110,  segmentMax = 220;    // Initial trunk lengths
+let minSegment = 110,  maxSegment = 220;    // Min/max trunk lengths
 
 // General variables
 let continents, country;
@@ -40,23 +41,21 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  // Initialize
+  // Set number of trees (one per continent)
   continents = populationTable.getColumnCount() - 1;
 
   // Generate Slider(s)
   sliders();
 
-  // Turn off continuous redraw
+  // Turn off continuous redraw. A redraw will the only occur if
+  // manually invoked (ie: from a slider value change)
   noLoop();
 
   // Cap lines with a round end (ROUND/SQUARE/PROJECT) and
   // Join lines using a round (MITER/BEVEL/ROUND) style
   // strokeCap(PROJECT); strokeJoin(BEVEL);
-  strokeCap(ROUND);
+  strokeCap(PROJECT);
   strokeJoin(ROUND);
-
-  // Draw ellipses from the upper left corner
-  ellipseMode(CORNER);
 }
 
 
@@ -71,33 +70,43 @@ function draw() {
 
   // Draw a tree for each continent and use -90° so that each tree points up
   for (let i = 1; i < continents + 1; i++) {
-    // Set which continent data to use for this tree
+
+    // Set the continent data to use for this tree
     setDependencyTable(i);
 
-    // Invoke a recursive tree function
+    // Invoke a recursive tree function, mapping the population of each
+    // continent for a given year to a minimum and maximum tree segment
     tree(i * width / (continents + 1), 0.88 * height,
       map(populationTable.getString(yearSlider.value() - 1960, i), 300000000,
-        4500000000, segmentMin, segmentMax), -90, maxBranchAngle);
+        4500000000, minSegment, maxSegment), -90, maxBranchAngle);
   }
 }
 
 
-// Recursively call the tree function, providing:
+// Recursive tree function, given:
 // xi          - Initial x value
 // yi          - Initial y value
 // length      - The length of the tree segment
 // angle       - The angle of the tree segment
 // branchAngle - The maximum angle of the next set of branches
 function tree(xi, yi, length, angle, branchAngle) {
-  if (length > 10) {
-    // The length of each segment ranges from a maximum of segmentMax to a minimum
+  if (length > minTrunkWidth) {
+    // The length of each segment ranges from a maximum of maxSegment to a minimum
     // of 3, which will correspond to a thickness of 15 and 0 respectively
-    stroke(50);
-    strokeWeight(map(length, segmentMax, 5, maxTrunkWidth, 0));
+    let weight = map(length, maxSegment, minTrunkWidth, maxTrunkWidth, 0);
 
-    // Calculate the final x,y values and draw a line between initial and final
+    // Calculate the final x,y values
     let xf = xi + cos(radians(angle)) * length;
     let yf = yi + sin(radians(angle)) * length;
+
+    // Add a leaf bud at the branch intersection
+    if (length > minSegment / 2) {
+      drawBud(xf, yf, weight);
+    }
+
+    // Draw a line between initial and final x, y values
+    stroke(50);
+    strokeWeight(weight);
     line(xi, yi, xf, yf);
 
     // Randomly determine new branch angles:
@@ -117,21 +126,25 @@ function tree(xi, yi, length, angle, branchAngle) {
       tree(xf, yf, newLengthR2, angle, branchAngle);
     }
   } else {
-    drawLeaf(xi, yi);
+    // Draw a leaf at the end point as a fuzzy elipse
+    colourLeaf(fillAlpha, strokeAlpha);
+    ellipse(xi, yi + (maxLeafHeight / 6), random(5, maxLeafWidth), random(20, maxLeafHeight));
   }
 }
 
 
-// Draw leaves as greenish circles
-function drawLeaf(x, y) {
+// Determine the leaf colour based on the age dependency ratio, given:
+// fAlpha - Fill alpha (transparency)
+// sAlpha - Stroke alpha (transparency)
+function colourLeaf(fAlpha, sAlpha) {
   // fill(random(100, 120), random(165, 185), random(130, 150), 20);
   // stroke(random(100, 120), random(165, 185), random(130, 150), 40);
   strokeWeight(0);
 
-  // Test that the country column number is within bounds
+  // Ensure that the country column number is within bounds
   country = country < dependencyTable.getColumnCount() ? country : 1;
 
-  // Get the corresponding dependency percentage and divide by 10
+  // Get the corresponding dependency ratio and divide by 10
   p = parseInt(dependencyTable.getString(yearSlider.value() - 1960, country) / 10);
 
   switch(p) {
@@ -140,51 +153,71 @@ function drawLeaf(x, y) {
     stroke(41, 106, 13, 40);
     break;
   case 1:
-    fill(60, 142, 23, 20);
-    stroke(60, 142, 23, 40);
+    fill(60, 142, 23, fAlpha);
+    stroke(60, 142, 23, sAlpha);
     break;
   case 2:
-    fill(119, 171, 12, 20);
-    stroke(119, 171, 12, 40);
+    fill(119, 171, 12, fAlpha);
+    stroke(119, 171, 12, sAlpha);
     break;
   case 3:
-    fill(186, 198, 0, 20);
-    stroke(186, 198, 0, 40);
+    fill(186, 198, 0, fAlpha);
+    stroke(186, 198, 0, sAlpha);
     break;
   case 4:
-    fill(223, 221, 25, 20);
-    stroke(223, 221, 25, 40);
+    fill(223, 221, 25, fAlpha);
+    stroke(223, 221, 25, sAlpha);
     break;
   case 5:
-    fill(250, 214, 51, 20);
-    stroke(250, 214, 51, 40);
+    fill(250, 214, 51, fAlpha);
+    stroke(250, 214, 51, sAlpha);
     break;
   case 6:
-    fill(250, 183, 51, 20);
-    stroke(250, 183, 51, 40);
+    fill(250, 183, 51, fAlpha);
+    stroke(250, 183, 51, sAlpha);
     break;
   case 7:
-    fill(255, 161, 44, 20);
-    stroke(255, 161, 44, 40);
+    fill(255, 161, 44, fAlpha);
+    stroke(255, 161, 44, sAlpha);
     break;
   case 8:
-    fill(255, 135, 44, 20);
-    stroke(255, 135, 44, 40);
+    fill(255, 135, 44, fAlpha);
+    stroke(255, 135, 44, sAlpha);
     break;
   case 9:
-    fill(254, 97, 44, 20);
-    stroke(254, 97, 44, 40);
+    fill(254, 97, 44, fAlpha);
+    stroke(254, 97, 44, sAlpha);
     break;
   default:
-    fill(253, 58, 45, 20);
-    stroke(253, 58, 45, 40);
+    fill(253, 58, 45, fAlpha);
+    stroke(253, 58, 45, sAlpha);
     break;
   }
 
-  ellipse(x, y, random(5, maxLeafWidth), random(20, maxLeafHeight));
-
-  // Increment country column number
+  // Increment the country column number
   country++;
+}
+
+
+// Draw an optional leaf bud
+function drawBud(x, y, w) {
+
+  // Set colour and stroke size
+  colourLeaf(255, 255);
+  strokeWeight(w / 5);
+
+  // Optionally draw on either the left or the right
+  switch(parseInt(random(3))) {
+  case 0:
+    ellipse(x - w / 2, y, w / 2, w / 2);
+    break;
+  case 1:
+    ellipse(x + w / 2, y, w / 2, w / 2);
+    break;
+  default:
+    // Do nothing
+    break;
+  }
 }
 
 
@@ -211,6 +244,7 @@ function setDependencyTable(i) {
     dependencyTable = samTable;
     break;
   default:
+    console.log("Invalid dependency table id: " + i);
     break;
   }
 
